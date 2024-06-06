547ca973aa821d1286cee11689db5489648b8495
Shindujaah Jaya Kumar <88871397+shindujaah@users.noreply.github.com>
2024-06-07 01:47:14 +0800

    Testing

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 import 'package:flutter/material.dart';
+// Testing
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
